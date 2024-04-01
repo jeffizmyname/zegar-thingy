ebe187ceb0cbd93519cff404bfa3c5cdd4b47691
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-04-01 14:23:41 +0200

    evryting in functions added from handling and generating clock from form

--- program.js
+++ program.js
@@ -1,90 +1,124 @@
+// the VARAIBLES
 let date = new Date();
 sec = document.getElementsByClassName("sec")
 min = document.getElementsByClassName("min")
 hour = document.getElementsByClassName("hour")
+secValues = null;
+minValues = null;
+hourValues = null;
 clock = document.getElementById("clock")
 numberOfPoints = 60
 centerX = 250
 centerY = 250
-r = 50
+r = 100
 x = 0
 y = 0
 padd = 20
 
-let point = date.getSeconds()
+//number generator function
+const gen = (numS, numM, numH, pad = padd, R = r) => {
+    clock.innerHTML = ""
+    padd = pad
+    r = R
+    for(let i = 0; i < numS; i++) {
+        let span = document.createElement("span")
+        span.classList.add("sec")
+        clock.appendChild(span);
+    }
 
-const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+    for(let i = 0; i < numM; i++) {
+        let span = document.createElement("span")
+        span.classList.add("min")
+        clock.appendChild(span);
+    }
 
-let secValues = Array.prototype.map.call(sec, function (el) {
-    return el;
-});
+    for(let i = 0; i < numH; i++) {
+        let span = document.createElement("span")
+        span.classList.add("hour")
+        clock.appendChild(span);
+    }
 
-let minValues = Array.prototype.map.call(min, function (el) {
-    return el;
-});
+    sec = document.getElementsByClassName("sec")
+    min = document.getElementsByClassName("min")
+    hour = document.getElementsByClassName("hour")
 
-let hourValues = Array.prototype.map.call(hour, function (el) {
-    return el;
-});
-
-secValues.forEach((element) => {
-    x = 250-r;
-    y = 250;
-    element.style.top = `${x}px`;
-    element.style.left = `${y}px`;
-    r+=padd
-})
-
-r = 100;
-
-minValues.forEach((element) => {
-    x = 250-r;
-    y = 250;
-    element.style.top = `${x}px`;
-    element.style.left = `${y}px`;
-    r+=padd
-})
-
-setInterval(() => {
-    r = 100
-    let startAngle = Math.PI
-    let secAngle = startAngle + (-date.getSeconds() / numberOfPoints) * 2 * Math.PI
-    let minAngle = startAngle + (-date.getMinutes() / numberOfPoints) * 2 * Math.PI
-    let hourAngle = startAngle + (-date.getHours() / numberOfPoints) * 2 * Math.PI
-
-    secValues.forEach((element) => {
-        x = (centerX + r * Math.cos(secAngle));
-        y = (centerY + r * Math.sin(secAngle));
-        element.style.top = `${x}px`;
-        element.style.left = `${y}px`;
-        element.innerHTML = date.getSeconds()
-        r+=padd
-    })
-
-    r = 100
-
-    minValues.forEach((element) => {
-        x = (centerX + r * Math.cos(minAngle));
-        y = (centerY + r * Math.sin(minAngle));
-        element.style.top = `${x}px`;
-        element.style.left = `${y}px`;
-        element.innerHTML = date.getMinutes()
-        r+=padd
-    })
-
-    r = 100
-
-    hourValues.forEach((element) => {
-        x = (centerX + r * Math.cos(hourAngle));
-        y = (centerY + r * Math.sin(hourAngle));
-        element.style.top = `${x}px`;
-        element.style.left = `${y}px`;
-        element.innerHTML = date.getHours()
-        r+=padd
-    })
+    secValues = Array.prototype.map.call(sec, function (el) {
+        return el;
+    });
     
-    date = new Date()
-}, 1000)
+    minValues = Array.prototype.map.call(min, function (el) {
+        return el;
+    });
+    
+    hourValues = Array.prototype.map.call(hour, function (el) {
+        return el;
+    });
+}
+
+// da logic updating numbers and position
+const runClock = () => {
+    let tempR = r
+    setInterval(() => {
+        tempR = r
+        let startAngle = Math.PI
+        let secAngle = startAngle + (-date.getSeconds() / numberOfPoints) * 2 * Math.PI
+        let minAngle = startAngle + (-date.getMinutes() / numberOfPoints) * 2 * Math.PI
+        let hourAngle = startAngle + (-date.getHours() / numberOfPoints) * 2 * Math.PI
+    
+        secValues.forEach((element) => {
+            x = (centerX + tempR * Math.cos(secAngle));
+            y = (centerY + tempR * Math.sin(secAngle));
+            element.style.top = `${x}px`;
+            element.style.left = `${y}px`;
+            element.innerHTML = date.getSeconds()
+            tempR+=padd
+        })
+    
+        tempR = r
+    
+        minValues.forEach((element) => {
+            x = (centerX + tempR * Math.cos(minAngle));
+            y = (centerY + tempR * Math.sin(minAngle));
+            element.style.top = `${x}px`;
+            element.style.left = `${y}px`;
+            element.innerHTML = date.getMinutes()
+            tempR+=padd
+        })
+    
+        tempR = r
+    
+        hourValues.forEach((element) => {
+            x = (centerX + tempR * Math.cos(hourAngle));
+            y = (centerY + tempR * Math.sin(hourAngle));
+            element.style.top = `${x}px`;
+            element.style.left = `${y}px`;
+            element.innerHTML = date.getHours()
+            tempR+=padd
+        })
+        
+        date = new Date()
+        console.log(r, tempR)
+    }, 1000)
+}
+
+//form handling
+const submitform = () => {
+    const secAmm = parseInt(document.getElementById("secAmm").value)
+    const minAmm = parseInt(document.getElementById("minAmm").value)
+    const hourAmm = parseInt(document.getElementById("hourAmm").value)
+
+    const padd = parseInt(document.getElementById("padding").value)
+    const radius = parseInt(document.getElementById("radius").value)
+
+    if(secAmm <= 0 || minAmm <= 0 || hourAmm <= 0 || padd <= 0 || radius <= 0) return -1;
+
+    gen(secAmm, minAmm, hourAmm, padd, radius)
+    runClock()
+}
+
+//da call
+gen(4, 4, 4)
+runClock()
 
 
 
